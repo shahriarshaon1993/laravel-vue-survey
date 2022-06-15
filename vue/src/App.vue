32f6e06753873ebc9975ffdffcb9d9afd7550419
Shahriar Shaon <shahriarshaon1993@gmail.com>
2022-06-15 11:17:30 +0600

    Add Tailwindcss fixing bug

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,30 +1,14 @@
-<script>
+<script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
-import {mapState} from 'vuex'
-
-export default {
-    computed: {
-      ...mapState(['user'])
-    }
-}
 
 </script>
 
 <template>
-  <pre>{{ user }}</pre>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <HelloWorld/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
