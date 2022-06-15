8554b0b41fc267f9b2fb9ebdd1b829272f8dc4ce
Shahriar Shaon <shahriarshaon1993@gmail.com>
2022-06-15 13:04:42 +0600

    Redirect To Login Page

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -5,7 +5,7 @@ import {createStore} from "vuex";
 const store = createStore({
     state: {
         user: {
-            data: {name: 'Shaon'},
+            data: {},
             token: null
         }
     },
