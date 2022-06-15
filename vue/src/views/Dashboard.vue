94d307daed9a1c8fc9e4ee11932ff49e29c0105e
Shahriar Shaon <shahriarshaon1993@gmail.com>
2022-06-15 12:17:21 +0600

    Vue Components (Login, Register, Layout)

--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -1,13 +1,40 @@
 <template>
-    <h1>Dashboard</h1>
+  <header class="bg-white shadow">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
+    </div>
+  </header>
+  <main>
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <!-- Replace with your content -->
+      <div class="px-4 py-6 sm:px-0">
+          Content goes that area
+      </div>
+      <!-- /End replace -->
+    </div>
+  </main>
 </template>
 
-<script>
-    export default {
-        name: "Dashboard"
+<script setup>
+    import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+    import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+
+    const user = {
+        name: 'Tom Cook',
+        email: 'tom@example.com',
+        imageUrl:
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     }
+    const navigation = [
+        { name: 'Dashboard', href: '#', current: true },
+        { name: 'Team', href: '#', current: false },
+        { name: 'Projects', href: '#', current: false },
+        { name: 'Calendar', href: '#', current: false },
+        { name: 'Reports', href: '#', current: false },
+    ]
+    const userNavigation = [
+        { name: 'Your Profile', href: '#' },
+        { name: 'Settings', href: '#' },
+        { name: 'Sign out', href: '#' },
+    ]
 </script>
-
-<style scoped>
-
-</style>
