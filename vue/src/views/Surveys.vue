a61ceac165437421286adbd6a946e87616ffd1b5
Shahriar Shaon <shahriarshaon1993@gmail.com>
2022-06-17 17:17:48 +0600

    Create Surveys Page

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,9 +1,29 @@
 <template>
-  <PageComponent title="Surveys">
-    Content goes that area
+  <PageComponent>
+    <template v-slot:header>
+      <div class="flex justify-between items-center">
+        <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
+        <router-link :to="{ name: 'SurveyCreate' }" class="py-2 px-3 text-white bg-emerald-500 rounded-md hover:bg-emerald-600">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+          </svg>
+          Add new survey
+        </router-link>
+      </div>
+    </template>
+
+    <div>
+      <pre>{{ surveys }}</pre>
+    </div>
+
   </PageComponent>
 </template>
 
 <script setup>
+  import store from "../store";
+  import { computed } from  "vue";
   import PageComponent from "../components/PageComponent.vue";
+
+  const surveys = computed(() => store.state.surveys);
+
 </script>
