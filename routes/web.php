e22bcfe5a6b3a5728fce2d273aa1b228b4d77a40
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2025-11-13 12:25:41 +0600

    laptop change

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/clear', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    return "Cleared!";
 });
